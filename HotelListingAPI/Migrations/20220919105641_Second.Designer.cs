cb912e8ac6efd452d20d35ee5d67f8346b5ba0d4
carciaag978 <58859680+carciaag978@users.noreply.github.com>
2022-09-19 13:58:11 +0300

    Branchu nou

--- HotelListingAPI/Migrations/20220919105641_Second.Designer.cs
+++ HotelListingAPI/Migrations/20220919105641_Second.Designer.cs
@@ -3,6 +3,7 @@ using HotelListingAPI.Date;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelListingAPI.Migrations
 {
     [DbContext(typeof(HotelListingDbContext))]
-    partial class HotelListingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220919105641_Second")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HotelListingAPI/Migrations/20220919105641_Second.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListingAPI.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShortName",
+                table: "Countries",
+                newName: "CountryShortName");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Countries",
+                newName: "CountryName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CountryShortName",
+                table: "Countries",
+                newName: "ShortName");
+
+            migrationBuilder.RenameColumn(
+                name: "CountryName",
+                table: "Countries",
+                newName: "Name");
+        }
+    }
+}
