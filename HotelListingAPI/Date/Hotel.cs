bb3eba105801b40d78a7afc98c1cd7670ad79f94
carciaag978 <58859680+carciaag978@users.noreply.github.com>
2022-09-19 13:35:37 +0300

    Added Entity Framework to the project and created database

---
+++ HotelListingAPI/Date/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListingAPI.Date
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+        public string HotelName { get; set; }
+        public string HotelAdress { get; set; }
+        public double HotelRating { get; set; }
+        [ForeignKey(nameof(CountryId))]
+        public int CountryId { get; set; }
+
+        public Country Country { get; set; }
+
+    }
+}
---
+++ HotelListingAPI/Date/HotelListingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingAPI.Date
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
